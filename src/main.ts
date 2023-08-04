d7c54c26a31bff4da12f23abaff8832aa322d656
市原紀葉 <ichihara_kazuha@cre-nagoya.com>
2023-08-04 11:14:53 +0900

    main.ts コメント

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
+//市原コメント致しました。
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
